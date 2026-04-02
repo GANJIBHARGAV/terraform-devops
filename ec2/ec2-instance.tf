89867c41057e7fccf5e65ab2363b24f77cf4eb41
Bhargav Ganji <bhargavganji08.com>
2026-04-02 18:45:44 +0530

    terraform

--- ec2/ec2-instance.tf
+++ ec2/ec2-instance.tf
@@ -1,12 +1,12 @@
-resource "aws_instance" "roboshop" {
-  ami           = "ami-0220d79f3f480ecf5"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_all.id]
+# resource "aws_instance" "roboshop" {
+#   ami           = "ami-0220d79f3f480ecf5"
+#   instance_type = "t2.micro"
+#   vpc_security_group_ids = [aws_security_group.allow_all.id]
 
-  tags = {
-    Name = "HelloWorld"
-  }
-}
+#   tags = {
+#     Name = "HelloWorld"
+#   }
+# }
 
 resource "aws_security_group" "allow_all" {
   name        = "allow_all"
